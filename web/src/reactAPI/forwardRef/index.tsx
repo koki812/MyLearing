0d9a23f3e81b01760d11855b434c6f62705a7dee
KOKI.K <koki.kong@sjfood.com>
2023-08-31 07:30:59 +0800

    update

--- web/src/reactAPI/forwardRef/index.tsx
+++
@@ -1,7 +0,0 @@
-import { forwardRef } from "react";
-
-export const ForwardRefDemo = forwardRef<HTMLDivElement, { text: string }>(
-    ({ text }, ref) => {
-        return <div ref={ref}>{text}</div>;
-    }
-);
--- web/src/reactAPI/memoized/index.tsx
+++
@@ -1,6 +0,0 @@
-import { memo } from "react";
-
-export const Memoized = memo(({ count }: { count: number }) => {
-    console.log("Rendering");
-    return <div>{count}</div>;
-});
